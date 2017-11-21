5f088c0c2ca1e2f10d3c520672ed5c190d8250fb
Vincent Endrahadi <13515117@std.stei.itb.ac.id>
2017-11-21 18:51:32 +0700

    Remove some error

--- src/index.js
+++ src/index.js
@@ -1,11 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
 import { BrowserRouter, Route } from "react-router-dom";
 import './css/index.css';
 import registerServiceWorker from './registerServiceWorker';
